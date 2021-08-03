2c0f5ac523b4832b4c8cc3f365de515e5fd4d508
Maria Eduarda <78294169+mariaEduarda-codes@users.noreply.github.com>
2021-08-03 20:30:33 -0300

    Create soma.c

---
+++ soma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x, y, soma;
+
+    printf("Digite o valor de X: ");
+    scanf("%i", &x);
+    printf("Digite o valor de Y: ");
+    scanf("%i", &y);
+    soma = x + y;
+    printf("SOMA = %i", soma);
+    return 0;
+}
